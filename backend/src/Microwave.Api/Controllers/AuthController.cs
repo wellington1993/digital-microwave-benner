293e5e9a6d5b47d3b33a2f1d741612f0cd60cda4
Wellington Torrejais da Silva <wtds.trabalho@gmail.com>
2026-03-11 18:10:00 -0300

    ajuste fino no mapeamento do login e tokens

--- backend/src/Microwave.Api/Controllers/AuthController.cs
+++ backend/src/Microwave.Api/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Microwave.Application.DTOs;
 using Microwave.Application.Services;
 
 namespace Microwave.Api.Controllers;
