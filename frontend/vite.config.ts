7300295f178cda96a832896e4cc2ab22634d2d3c
Wellington Torrejais da Silva <wtds.trabalho@gmail.com>
2026-03-11 14:35:00 -0300

    configurando usePolling no vite para melhorar hot reload no windows

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,20 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
   server: {
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    hmr: {
+      overlay: true,
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -13,10 +22,5 @@ export default defineConfig({
         secure: false,
       }
     }
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
   },
 })
